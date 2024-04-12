4e328c5812320227ee377290dfbae52c693e1e0e
da4 <da4@accessdenied.org>
2024-04-12 12:27:26 -0500

    Create DBeaver - Set Usage Collection.sh

---
+++ DBeaver - Set Usage Collection.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set DBeaver pref to disable usage collection
+# even though DBeaver publishes their policy and claim to anonymize all data
+# some orgs may need to disable this setting
+
+currentUser=$( /usr/bin/stat -f%Su "/dev/console" )
+currentUserHome=$(/usr/bin/dscl . -read "/Users/$currentUser" NFSHomeDirectory | /usr/bin/awk ' { print $NF } ')
+
+prefs="${currentUserHome}"/Library/DBeaverData/workspace6/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.jkiss.dbeaver.ui.statistics.prefs
+
+if [[ -e "${prefs}" ]]; then
+
+	/usr/bin/sed -i '' 's/enabled=true/enabled=false/' "${prefs}"
+    
+fi
