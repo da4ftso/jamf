4eecbff73e9c50b3bfe2abd5e320025ca803c882
da4 <da4@accessdenied.org>
2025-04-02 08:38:11 -0700

    Add files via upload

---
+++ edgeupdater.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# EdgeUpdater-Cleanup-1.0.1
+# 250402 PWC
+
+updateDir="/Library/Application Support/Microsoft/EdgeUpdater/apps"
+
+if [[ -d "${updateDir}" ]]; then
+        space=$(du -h "${updateDir}" | tail -1 | awk ' { print $1 }')
+        rm -rf "${updateDir:?}"/*
+        echo "Removed ""$space"" of old updates.."
+fi
+
+exit
