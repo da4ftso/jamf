82d61f806b0f273160f4c75bd112a2947b89b2e9
da4 <da4@accessdenied.org>
2025-05-05 14:48:22 -0500

    Create Wireshark uninstall.sh
    1.0.2

---
+++ Wireshark uninstall.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Remove the following:
+# - Wireshark.app from /Applications
+# - ChmmodBPF launch daemon & script
+# - access_bpf group from dscl
+# - HTTPStorages folder
+# - system package receipts (leaving Jamf receipts)
+
+# variables
+
+currentUser=$(stat -f %Su "/dev/console")
+currentUserHome=$(dscl . read "/Users/$currentUser" NFSHomeDirectory | awk ' { print $NF } ')
+
+# remove LaunchDaemon
+
+CHMOD_BPF_PLIST="/Library/LaunchDaemons/org.wireshark.ChmodBPF.plist"
+BPF_GROUP="access_bpf"
+
+launchd=$(launchctl list | grep -iE wireshark)
+if [[ -n "$launchd" ]]; then
+	launchctl bootout system "$CHMOD_BPF_PLIST"
+fi    
+
+# remove group from dscl
+
+dscl . -read /Groups/"$BPF_GROUP" > /dev/null 2>&1 && \
+    dseditgroup -q -o delete "$BPF_GROUP" 2>&1
+
+# remove files
+# ChmodBPF hasn't been a startup item since 2018 (ac4f3c0f4d)
+
+list=(
+"/Applications/Wireshark.app"
+"/Library/Application Support/Wireshark"
+"/Library/LaunchDaemons/org.wireshark.ChmodBPF.plist"
+"/Library/StartupItems/ChmodBPF"
+"$currentUserHome/Library/HTTPStorages/org.wireshark.Wireshark/"
+)
+
+for i in "${list[@]}"; do
+        if [[ -e $i ]]; then
+                rm -rf "$i" || exit
+                echo "Removing $i .."
+        fi  
+done    
+
+# https://gitlab.com/wireshark/wireshark/-/issues/18734
+pkgutil --forget org.wireshark
+pkgutil --forget org.wireshark.ChmodBPF.pkg
