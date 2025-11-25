7e415abc1dd68d72d4f4c86f5b92aa3706ff4969
da4 <da4@accessdenied.org>
2025-11-25 09:36:31 -0600

    Update cscan_fix.sh
    pkill cscan as last step

--- cscan_fix.sh
+++ cscan_fix.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
+set -x
 
 PARENT_PID=$(pgrep -f "cscan")
 
 if [ -z "$PARENT_PID" ]; then
-
-  echo "No process found with the name: $PROCESS_NAME"
-  exit 1
-
+  echo "No process found with the name: cscan"
+  exit 0
 fi
 
 # echo "Parent PID: $PARENT_PID"
-
-# Find all child PIDs of the parent process
+# find all child PIDs of the parent process
 
 CHILD_PIDS=$(ps -eo pid,ppid | awk -v ppid="$PARENT_PID" '$2 == ppid {print $1}')
- 
 
 if [ -z "$CHILD_PIDS" ]; then
-
   echo "No child processes found for PID: $PARENT_PID"
   exit 0
-
 fi
 
+count=$(echo "CHILD_PIDS" | wc | awk ' { print $2 } ')
 
-echo "Child PIDs: $CHILD_PIDS"
-
+# echo "Child PIDs: $CHILD_PIDS"
 
 # Kill all child processes
 
 for pid in $CHILD_PIDS; do
-
-  echo "Killing child process with PID: $pid"
-  kill -9 $pid
-
+#  echo "Killing child process with PID: $pid"
+  kill -9 "$pid"
 done
 
+echo "$count"" child processes of PID $PARENT_PID killed."
 
-echo "All child processes of PID $PARENT_PID have been killed."
+pkill $PARENT_PID
