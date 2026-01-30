703efa0fe36be23a4db3fc762ecab252fb29781f
da4 <da4@accessdenied.org>
2026-01-30 14:34:31 -0600

    Update MyEclipse 2025 - Install.sh
    gave up on parsing PIDs, just sleep it off

--- MyEclipse 2025 - Install.sh
+++ MyEclipse 2025 - Install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -x
 
-# 1.0.1 unattended install for MyEclipse 2025.12 - cloned from the 2022 version & improved
+# 1.0.4 unattended install for MyEclipse 2025.12 - cloned from the 2022 version & improved
 #
 # TO-DO: change from hardcoded installer version and pass param 4
 #    (but that will require some detection logic to verify DMG)
 #  more logging
-#  unmount the volume after standard-install has completed
-#  clean-up the DMG
-#  clean up the response.txt
 
 # variables
 
@@ -23,25 +20,50 @@ if [ ! -e "${cacheDir}"/$file ]; then
     exit 1
 fi
 
-# mount up
+# mount up - removing the nobrowse during testing
 
-/usr/bin/hdiutil mount -nobrowse -quiet "${cacheDir}"/$file
+# /usr/bin/hdiutil mount -nobrowse -quiet "${cacheDir}"/$file
+/usr/bin/hdiutil mount -quiet "${cacheDir}"/$file
 sleep 2
 
 # cat out the response.txt https://www.genuitec.com/forums/topic/unattended-installation/
 
-cat << EOF > "/private/var/tmp/response.txt"
-directory.profile=/Applications/MyEclipse
-osgi.os=macosx
-osgi.ws=cocoa
-osgi.arch=x86_64
-result.file=/Users/Shared/myeclipse-install.log
-
-EOF
+cat <<-EOF > "/private/var/tmp/response.txt"
+	directory.profile=/Applications/MyEclipse
+	osgi.os=macosx
+	osgi.ws=cocoa
+	osgi.arch=x86_64
+	result.file=/Users/Shared/myeclipse-install.log
+	EOF
 
 # install (automatic but not silent)
 
-/usr/bin/sudo -u "$currentUser" /Volumes/MyEclipse/MyEclipse\ 2025.2.0\ Installer.app/Contents/MacOS/standard-install --unattended /private/var/tmp/response.txt
+/usr/bin/sudo -u "$currentUser" /Volumes/MyEclipse/MyEclipse\ 2025.2.0\ Installer.app/Contents/MacOS/standard-install \
+--unattended /private/var/tmp/response.txt
 
+# unmount the volume after standard-install has completed
+#
+# the below doesn't seem to work?
+# while pgrep -f standard-install > /dev/null ; do
+# 	sleep 2
+# done
+
+# pids=$(pgrep -f standard-install || true)
+# if [ -z "$pids" ]; then
+# 	echo "No installer running.."
+# else
+# 	while kill -0 $(echo $pids)	2>/dev/null; do
+# 		sleep 2
+# 	done
+# 	echo "standard-install has completed, unmounting.."	
+# 	/usr/bin/hdiutil unmount /Volumes/MyEclipse
+# fi	
+
+sleep 90 # about a minute on modern systems, but pad it jic
+
+/usr/bin/hdiutil unmount /Volumes/MyEclipse	
+
+rm -rf "${cacheDir}"/$file
+rm /private/var/tmp/response.txt
 
 exit 0
