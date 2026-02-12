5e99fb5301cf375f61a25b2f9b219bdb3346ad27
da4 <da4@accessdenied.org>
2026-02-12 10:47:24 -0600

    Create Anypoint Studio - Install.sh
    1.0

---
+++ Anypoint Studio - Install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# TO-DO:
+# replace shebang with the better env version
+# pass filename as param 4, validate
+
+
+# this assumes that you have cached AnypointStudio-VER-ARCH.dmg to Waiting Room
+
+# variables
+currentUser=$( stat -f%Su /dev/console ) 
+
+# mount
+hdiutil mount /Library/Application\ Support/JAMF/Waiting\ Room/AnypointStudio-7.24.0-macosArm.dmg
+
+# ditto doesn't seem to work reliably, neither does cp?
+rsync -avhPrq /Volumes/AnypointStudio/AnypointStudio.app /Applications/ 
+
+# own it
+chown -R $currentUser /Applications/AnypointStudio.app 
+
+# dismount
+hdiutil unmount /Volumes/AnypointStudio 
+
+# cleanup
+rm /Library/Application\ Support/JAMF/Waiting\ Room/AnypointStudio*
