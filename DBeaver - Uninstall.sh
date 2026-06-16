b8f05b9735f88ad4df40f08de6d62273e367ce11
da4 <da4@accessdenied.org>
2026-06-16 09:11:16 -0500

    Create DBeaver - Uninstall.sh

---
+++ DBeaver - Uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# set env pipefail here?
+
+# Remove files and directories
+files_to_remove=(
+	"/Applications/DBeaver.app"
+)
+
+for f in "${files_to_remove[@]}"; do
+	if [[ -e "$f" ]]; then
+		echo "Deleting: $f"
+		rm -Rf "$f"
+	fi
+done
+
+echo "Uninstall complete."
+exit 0
