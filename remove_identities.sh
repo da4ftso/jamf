4fab6de7d61810574cad7d023f7b627413980c08
da4 <da4@accessdenied.org>
2026-07-10 13:20:18 -0500

    Enhance comments for clarity in remove_identities.sh
    Add comments to clarify the purpose and usage of the script.

--- remove_identities.sh
+++ remove_identities.sh
@@ -4,12 +4,16 @@
 # remove third identity and all identities after the third
 # to help resolve 802.1x network authentication issues on macOS Tahoe
 
+
+
 # example output:
 # # security find-identity -v
 #   1) D51CFC1F8A6881B9EA3091A02CBC0807E21A644E "EAE773AF-F6C6-4667-93A9-B4D0D26A8D2A"
 #   2) 7789D768D5BD10C8A4A420BB903E110720F06B01 "U446852"
 #   3) 94178EFA5C5DF8BDBB9CACBA0EE6264D0118BCE1 "U446852"
 #      3 valid identities found
+
+# 'security find-identity -p basic' will also return CSSMERR_TP_CERT_EXPIRED
 
 # Get all identities and extract those numbered 3 or higher
 security find-identity -v | grep -E '^\s+[0-9]+\)' | awk '$1 >= 3 {print $1}' | while read -r identity_num; do
