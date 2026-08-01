55372ec28037656dc4d2a26558da27890c71ccdc
da4 <da4@accessdenied.org>
2026-08-01 10:00:04 -0500

    Update uninstall script with additional comments
    Added comments for clarity and reference to a more robust uninstall script.

--- uninstall/Office 2016 - Uninstall.sh
+++ uninstall/Office 2016 - Uninstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this is a (very old) basic version without any jamf params for options - for a more robust version, try:
+# https://github.com/cocopuff2u/MOFA/blob/main/office_reset_tools/mofa_community_maintained/scripts/MOFA_Community_Microsoft_Office_Removal.zsh
+
 # minor edits based on shellcheck and better $user checking
 
 consoleuser=$(stat -f %Su /dev/console);
