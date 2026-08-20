88b51d279f025c1d128a5ac85040a3b958f815a6
da4 <da4@accessdenied.org>
2026-08-20 12:35:53 -0500

    Create OneDrive By IP.sh

---
+++ OneDrive By IP.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OnNetwork=0
+
+if ifconfig | grep -E 'inet 10.' >/dev/null; then
+    OnNetwork=1
+fi
+
+if [[ -x /opt/cisco/secureclient/bin/vpn ]]; then
+    if /opt/cisco/secureclient/bin/vpn state 2>/dev/null | grep -q "Connected"; then
+        OnNetwork=1
+    fi
+fi
+
+if [[ $OnNetwork -eq 1 ]]; then
+    open -a OneDrive
+else
+    osascript -e 'tell application "OneDrive" to quit'
+fi
+
+exit 0
