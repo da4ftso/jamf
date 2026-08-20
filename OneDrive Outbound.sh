5c73b168994a3999bd64a5f3158d6be5b988ae5b
da4 <da4@accessdenied.org>
2026-08-20 12:01:39 -0500

    Create OneDrive Outbound.sh

---
+++ OneDrive Outbound.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Quit OneDrive if it cannot establish a sync connection
+
+CONSOLE_USER=$(stat -f "%Su" /dev/console)
+CONSOLE_UID=$(id -u "${CONSOLE_USER}")
+
+if ! pgrep -x "OneDrive" >/dev/null; then
+exit 0
+fi
+
+for i in {1..6}; do
+    if lsof -nP -a -c OneDrive -iTCP -sTCP:ESTABLISHED 2>/dev/null |
+        grep -q -- '->.*:443'; then
+        exit 0
+    fi
+
+    sleep 5
+done
+
+launchctl asuser "${CONSOLE_UID}" \
+    osascript -e 'tell application "OneDrive" to quit' >/dev/null 2>&1
+
+exit 0
