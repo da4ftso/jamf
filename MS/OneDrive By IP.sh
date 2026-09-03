f12801df7571c687688e6d9fdcbe7b193e1de1e2
da4 <da4@accessdenied.org>
2026-09-03 13:46:14 -0500

    Update and rename OneDrive By IP.sh to MS/OneDrive By IP.sh

--- MS/OneDrive By IP.sh
+++ MS/OneDrive By IP.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# OneDrive connectivity check
+# use with upgrades, shortcuts etc
+
 OnNetwork=0
 
+# assuming corp IP space with 10.x.x.x 
 if ifconfig | awk '/inet / && $2 ~ /^10\./ { found=1 } END { exit !found }'; then
     OnNetwork=1
 fi
 
+# or maybe we're on the VPN
 if [[ -x /opt/cisco/secureclient/bin/vpn ]]; then
     if /opt/cisco/secureclient/bin/vpn state 2>/dev/null | grep -q "Connected"; then
         OnNetwork=1
     fi
 fi
 
+# open -a tries to keep from app from opening in foreground
 if [[ $OnNetwork -eq 1 ]]; then
     open -a OneDrive
 else
